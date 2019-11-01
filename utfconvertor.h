e015f36c5134072591aee5cf8ccdfc273e007f18
willem <itsme@xs4all.nl>
2019-11-01 11:08:01 +0100

    all functions now take start+end for both src and dst. added countneeded to all conversions.

--- utfconvertor.h
+++ utfconvertor.h
@@ -1,10 +1,9 @@
 #pragma once
 
+#include "utfcvutils.h"
+#include <tuple>
+
 namespace {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wunused-function"
-#include "utfcvutils.cpp"
-#pragma clang diagnostic pop
 
     /* anonymous namespace for translating char types to types defined by utfcvutils.h */
 
@@ -21,14 +20,11 @@ template<> struct unsignedforsize<8> { typedef uint64_t type; };
 
 /* basically stringcopy, taking care of terminating NUL, returning used items */
 template<typename D, typename S>
-size_t identityconvert(const S* src, D* dst, size_t maxsize)
+auto identityconvert(S src, S send, D dst, D dend)
 {
-    D* p= dst;
-    D* pend= dst+maxsize-1;
-    while (*src && p<pend)
-        *p++ = *src++;
-    *p = 0;
-    return p-dst;
+    while (src<send && dst<dend)
+        *dst++ = *src++;
+    return std::make_tuple(src, dst);
 }
 }
 
@@ -66,21 +62,25 @@ const typename chartypes<TO>::CHAR* cast_to_char(const T *p)
  */
 template<int FROM, int TO>
 struct utfconvertor {
-    // size_t convert(*src, *dst, maxsize);
+    // [ sused, dused ] convert(*src, *dst, maxsize);
     // size_t maxsize(size_t from)
 };
 
 template<>
 struct utfconvertor<1,2> {  enum { FROM=1, TO=2 };
-    template<typename SRC, typename DST>
-    static size_t convert(const SRC *src, DST *dst, size_t maxsize) { return utf8toutf16(char_cast(src), char_cast(dst), maxsize); }
+    template<typename D, typename S>
+    static auto convert(S src, S send, D dst, D dend) { return utf8toutf16(src, send, dst, dend); }
+    template<typename S>
+    static auto countneeded(S src, S end) { return utf8toutf16needed(src, end); }
 
     static size_t maxsize(size_t from) { return from; }
 };
 template<>
 struct utfconvertor<2,1> {  enum { FROM=2, TO=1 };
-    template<typename SRC, typename DST>
-    static size_t convert(const SRC *src, DST *dst, size_t maxsize) { return utf16toutf8(char_cast(src), char_cast(dst), maxsize); }
+    template<typename D, typename S>
+    static auto convert(S src, S send, D dst, D dend) { return utf16toutf8(src, send, dst, dend); }
+    template<typename S>
+    static auto countneeded(S src, S end) { return utf16toutf8needed(src, end); }
 
     // for values between 0x800 and 0xffff you need 3 bytes in utf-8, and only 2 in utf-16
     static size_t maxsize(size_t from) { return 3*from; }
@@ -88,52 +88,70 @@ struct utfconvertor<2,1> {  enum { FROM=2, TO=1 };
 
 template<>
 struct utfconvertor<1,4> {  enum { FROM=1, TO=4 };
-    template<typename SRC, typename DST>
-    static size_t convert(const SRC *src, DST *dst, size_t maxsize) { return utf8toutf32(char_cast(src), char_cast(dst), maxsize); }
+    template<typename D, typename S>
+    static auto convert(S src, S send, D dst, D dend) { return utf8toutf32(src, send, dst, dend); }
+    template<typename S>
+    static auto countneeded(S src, S end) { return utf8toutf32needed(src, end); }
 
     // values below 0x80 require 4 times more size in utf-32
     static size_t maxsize(size_t from) { return from; }
 };
 template<>
 struct utfconvertor<4,1> {  enum { FROM=4, TO=1 };
-    template<typename SRC, typename DST>
-    static size_t convert(const SRC *src, DST *dst, size_t maxsize) { return utf32toutf8(char_cast(src), char_cast(dst), maxsize); }
+    template<typename D, typename S>
+    static auto convert(S src, S send, D dst, D dend) { return utf32toutf8(src, send, dst, dend); }
+    template<typename S>
+    static auto countneeded(S src, S end) { return utf32toutf8needed(src, end); }
 
     // values above 0x10000 require 4 utf-8 bytes
     static size_t maxsize(size_t from) { return 4*from; }
 };
 template<>
 struct utfconvertor<4,2> {  enum { FROM=4, TO=2 };
-    template<typename SRC, typename DST>
-    static size_t convert(const SRC *src, DST *dst, size_t maxsize) { return utf32toutf16(char_cast(src), char_cast(dst), maxsize); }
+    template<typename D, typename S>
+    static auto convert(S src, S send, D dst, D dend) { return utf32toutf16(src, send, dst, dend); }
+    template<typename S>
+    static auto countneeded(S src, S end) { return utf32toutf16needed(src, end); }
 
     // values above 0x10000 require 2 utf-16 bytes
     static size_t maxsize(size_t from) { return 2*from; }
 };
 template<>
 struct utfconvertor<2,4> {  enum { FROM=2, TO=4 };
-    template<typename SRC, typename DST>
-    static size_t convert(const SRC *src, DST *dst, size_t maxsize) { return utf16toutf32(char_cast(src), char_cast(dst), maxsize); }
+    template<typename D, typename S>
+    static auto convert(S src, S send, D dst, D dend) { return utf16toutf32(src, send, dst, dend); }
+    template<typename S>
+    static auto countneeded(S src, S end) { return utf16toutf32needed(src, end); }
 
     static size_t maxsize(size_t from) { return from; }
 };
 template<>
 struct utfconvertor<1,1> {  enum { FROM=1, TO=1 };
-    template<typename SRC, typename DST>
-    static size_t convert(const SRC *src, DST *dst, size_t maxsize) { return identityconvert(src, dst, maxsize); }
+    template<typename D, typename S>
+    static auto convert(S src, S send, D dst, D dend) { return identityconvert(src, send, dst, dend); }
+    template<typename S>
+    static auto countneeded(S src, S end) { return std::distance(src, end); }
+
     static size_t maxsize(size_t from) { return from; }
 };
 template<>
 struct utfconvertor<2,2> {  enum { FROM=2, TO=2 };
-    template<typename SRC, typename DST>
-    static size_t convert(const SRC *src, DST *dst, size_t maxsize) { return identityconvert(src, dst, maxsize); }
+    template<typename D, typename S>
+    static auto convert(S src, S send, D dst, D dend) { return identityconvert(src, send, dst, dend); }
+    template<typename S>
+    static auto countneeded(S src, S end) { return std::distance(src, end); }
+
     static size_t maxsize(size_t from) { return from; }
 };
 template<>
 struct utfconvertor<4,4> {  enum { FROM=4, TO=4 };
-    template<typename SRC, typename DST>
-    static size_t convert(const SRC *src, DST *dst, size_t maxsize) { return identityconvert(src, dst, maxsize); }
+    template<typename D, typename S>
+    static auto convert(S src, S send, D dst, D dend) { return identityconvert(src, send, dst, dend); }
+    template<typename S>
+    static auto countneeded(S src, S end) { return std::distance(src, end); }
+
     static size_t maxsize(size_t from) { return from; }
 };
+
 
 
