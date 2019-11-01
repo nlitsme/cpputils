91693d48e27fa0bb107bca3937df8d0d4db3d747
willem <itsme@xs4all.nl>
2019-11-01 11:08:01 +0100

    stringconvert: changed to handle both NUL terminated and length specified strings. also now handled more c++ string types.

--- stringconvert.h
+++ stringconvert.h
@@ -14,30 +14,8 @@
  * (C) 2016  Willem Hengeveld <itsme@xs4all.nl>
  *
  */
-
-// fixed to UTFCV
-#define USE_UTFCV 1
-
 #include <string>
-
-
-// stringconver can use three different libraries for the actual conversions:
-//    boost/locale.hpp, libstdc++ codecvt, or my own utfconvertor.h
-
-#ifdef USE_BOOST_LOCALE
-//  adds dependency on boost-locale lib
-#include <boost/locale.hpp>
-
-#elif defined(USE_CODECVT)
-// does not work properly - due to lack of codecvt_utf8<T,T> templates
-// see http://www.italiancpp.org/2016/04/20/unicode-localization-and-cpp-support/
-// note: currently doing  SRC -> utf8 -> DST, instead of directly converting from SRC to DST.
-#include <codecvt>   // codecvt_utf8
-#include <locale>    // wstring_convert
-
-#elif defined(USE_UTFCV)
 #include "utfconvertor.h"
-#endif
 
 namespace string {
 
@@ -59,48 +37,64 @@ size_t length(const T *str)
 }
 
 
-/////////////////////////////////////////////////////////
-// convert std::string type strings
-template<typename DST, typename SRC>
-inline std::basic_string<DST> convert(const std::basic_string<SRC>& src)
+/**
+ * should take any kind of container: string, vector, array or stringview
+ */
+template<typename DSTCHAR, typename SRC>
+inline auto convert(const SRC& src)
 {
-#ifdef USE_BOOST_LOCALE
-    return boost::locale::conv::utf_to_utf<DST>(src);
-#elif defined(USE_CODECVT)
-    // note: problem - codecvt_utf8<char> is missing
-    std::wstring_convert<std::codecvt_utf8<SRC>,SRC> fromsrc;// widestring<SRC> -> char
-    std::wstring_convert<std::codecvt_utf8<DST>,DST> todst;  // char -> widestring<DST>
+    using SRCCHAR = typename SRC::value_type;
 
-    return todst.from_bytes(fromsrc.to_bytes(src));
-#elif defined(USE_UTFCV)
-    std::basic_string<DST> dst(utfconvertor<sizeof(SRC), sizeof(DST)>::maxsize(src.size())+1, DST(0));
+    std::basic_string<DSTCHAR> dst(utfconvertor<sizeof(SRCCHAR), sizeof(DSTCHAR)>::maxsize(std::size(src)), DSTCHAR(0));
 
-    /*size_t used =*/ utfconvertor<sizeof(SRC), sizeof(DST)>::convert(src.c_str(), &dst[0], dst.size());
-    dst.resize(z::length(dst.c_str()));
+    auto [ sused, dused ] = utfconvertor<sizeof(SRCCHAR), sizeof(DSTCHAR)>::convert(std::begin(src), std::end(src), std::begin(dst), std::end(dst));
+
+    dst.erase(dused, dst.end());
     return dst;
-#endif
 }
 
-
-/////////////////////////////////////////////////////////
-// convert C NUL terminated type strings
-template<typename DST, typename SRC>
-inline std::basic_string<DST> convert(const SRC* src)
+/**
+ *  takes a NUL terminated C type string.
+ */
+template<typename DSTCHAR, typename SRC>
+inline auto convert(const SRC* src)
 {
-#ifdef USE_BOOST_LOCALE
-    return boost::locale::conv::utf_to_utf<DST>(src);
-#elif defined(USE_CODECVT)
-    std::wstring_convert<std::codecvt_utf8<SRC>,SRC> fromsrc;// widestring<SRC> -> char
-    std::wstring_convert<std::codecvt_utf8<DST>,DST> todst;  // char -> widestring<DST>
+    auto l = z::length(src);
+    std::basic_string<DSTCHAR> dst(utfconvertor<sizeof(SRC), sizeof(DSTCHAR)>::maxsize(l), DSTCHAR(0));
 
-    return todst.from_bytes(fromsrc.to_bytes(src));
-#elif defined(USE_UTFCV)
-    std::basic_string<DST> dst(utfconvertor<sizeof(SRC), sizeof(DST)>::maxsize(z::length(src))+1, DST(0));
+    auto [ sused, dused ] = utfconvertor<sizeof(SRC), sizeof(DSTCHAR)>::convert(src, src + l, std::begin(dst), std::end(dst));
 
-    /*size_t used =*/ utfconvertor<sizeof(SRC), sizeof(DST)>::convert(src, &dst[0], dst.size());
-    dst.resize(z::length(dst.c_str()));
+    dst.erase(dused, dst.end());
     return dst;
-#endif
+}
+/**
+ *  takes a pointer and a length
+ */
+template<typename DSTCHAR, typename SRC>
+inline auto convert(const SRC* src, size_t length)
+{
+    std::basic_string<DSTCHAR> dst(utfconvertor<sizeof(SRC), sizeof(DSTCHAR)>::maxsize(length), DSTCHAR(0));
+
+    auto [ sused, dused ] = utfconvertor<sizeof(SRC), sizeof(DSTCHAR)>::convert(src, src + length, std::begin(dst), std::end(dst));
+
+    dst.erase(dused, dst.end());
+    return dst;
+}
+
+/**
+ *  takes iterator or pointer pair.
+ */
+template<typename DSTCHAR, typename SRCPTR>
+inline auto convert(SRCPTR first, SRCPTR last)
+{
+    using SRCCHAR = typename std::iterator_traits<SRCPTR>::value_type;
+
+    std::basic_string<DSTCHAR> dst(utfconvertor<sizeof(SRCCHAR), sizeof(DSTCHAR)>::maxsize(std::distance(first, last)), DSTCHAR(0));
+
+    auto [ sused, dused ] = utfconvertor<sizeof(SRCCHAR), sizeof(DSTCHAR)>::convert(first, last, std::begin(dst), std::end(dst));
+
+    dst.erase(dused, dst.end());
+    return dst;
 }
 
 #ifdef _WIN32
@@ -117,5 +111,5 @@ inline std::basic_string<DST> convert(Platform::String^ps)
 }
 #endif
 #endif
-} // namespace
 
+} // namespace
