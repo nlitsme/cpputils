ca2d2e862701dfc71e39e8830aabb9d761130991
willem <itsme@xs4all.nl>
2020-01-21 11:54:37 +0100

    mmfile: wraps both a filehandle and mmap

---
+++ mmfile.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <fcntl.h>
+
+#include "mmem.h"
+#include "fhandle.h"
+
+/* retain both the filehandle and the mmap */
+class mappedfile {
+    filehandle _f;
+    mappedmem _m;
+public:
+    mappedfile(filehandle& fh, int flags = O_RDONLY)
+        : _f(fh),
+        _m(_f, 0, _f.size(), (flags==O_RDONLY)?PROT_READ:(flags==O_WRONLY)?PROT_WRITE:(flags==O_RDWR)?(PROT_READ|PROT_WRITE):0)
+    {
+    }
+
+    mappedfile(const std::string& filename, int openflags = O_RDONLY)
+        : mappedfile(open(filename.c_str(), openflags))
+    {
+    }
+    mappedfile(int fh, int mmapmode = PROT_READ)
+        : mappedfile(filehandle(fh), mmapmode)
+    {
+    }
+
+
+    auto size() { return _m.size(); }
+    auto begin() { return _m.begin(); }
+    auto end() { return _m.end(); }
+};
+
