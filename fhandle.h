6b00c8dc16f6022b91d1fb2503abb3c47f4b1532
willem <itsme@xs4all.nl>
2020-01-21 11:54:37 +0100

    fhandle: now keeps a sharedptr to the actual filehandle, so filehandles can be copied. added seek, tell, trunc, write, read methods

--- fhandle.h
+++ fhandle.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <memory>
+#include <vector>
 #include <stdexcept>
 #include <system_error>
 
@@ -19,56 +21,74 @@
 #include <unistd.h>
 #endif
 
-/*
- * NOTE: problem on macosx: you can't mmap a block or char device.
- *
- * see https://stackoverflow.com/questions/24520474/mmap-a-block-device-on-mac-os-x
- *
- * the problem is in the mmap kernel driver:
-https://opensource.apple.com/source/xnu/xnu-2422.1.72/bsd/kern/kern_mman.c
-
-        if (vp->v_type != VREG && vp->v_type != VCHR) {
-			(void)vnode_put(vp);
-			error = EINVAL;
-			goto bad;
-		}
-        ...
-		if (vp->v_type == VCHR || vp->v_type == VSTR) {
-			(void)vnode_put(vp);
-			error = ENODEV;
-			goto bad;
-		} 
-
- */
 // wrap posix file handle in a c++ class,
 // so it will be closed when it leaves the current scope.
 // This is most useful for making code exception safe.
 
-// NOTE: this object should not be copied.
 
 struct filehandle {
-    int f=-1;
+
+    struct ptr {
+        int fh = -1;
+        ptr(int fh)
+            : fh(fh)
+        {
+        }
+        ~ptr() { close(); }
+
+        void close()
+        {
+            if (fh==-1)
+                return;
+            if (-1 == ::close(fh)) {
+                fh = -1;
+                throw std::system_error(errno, std::generic_category(), "close");
+            }
+            fh = -1;
+        }
+    };
+    std::shared_ptr<ptr> _p;
+
 
     filehandle() { }
 
-    filehandle(int f) : f(f) { 
-        if (f==-1) {
-            throw std::system_error(errno, std::generic_category());
-        }
+    filehandle(filehandle& fh)
+    {
+        *this = fh;
     }
 
-    // disallow copying - there may only be one owner.
-    filehandle(const filehandle & fh) = delete;
+    filehandle(int fh)
+    {
+        *this = fh;
+    }
+    filehandle& operator=(int fh)
+    { 
+        if (fh==-1)
+            throw std::system_error(errno, std::generic_category());
+        _p = std::make_shared<ptr>(fh);
+        return *this;
+    }
+    filehandle& operator=(filehandle& fh)
+    {
+        _p = fh._p;
+        return *this;
+    }
+    bool empty() { return !_p; }
 
-    ~filehandle() { close(); }
-    filehandle& operator=(int fh) { f= fh; return *this; }
-    operator int () const { return f; }
-    void close() { if (f!=-1) ::close(f); }
+    operator int () const { return fh(); }
+    int fh() const {
+        if (!_p) throw std::runtime_error("no filehandle set");
+        return _p->fh;
+    }
+    void close() {
+        if (!_p) throw std::runtime_error("no filehandle set");
+        _p->close();
+    }
 
     int64_t size()
     {
         struct stat st;
-        if (fstat(f, &st))
+        if (fstat(fh(), &st))
             return -1;
         if (st.st_mode&S_IFREG)
             return st.st_size;
@@ -76,15 +96,15 @@ struct filehandle {
 #ifdef DKIOCGETBLOCKCOUNT
             uint64_t bkcount;
             uint32_t bksize;
-            if (-1==ioctl(f, DKIOCGETBLOCKCOUNT, &bkcount))
+            if (-1==ioctl(fh(), DKIOCGETBLOCKCOUNT, &bkcount))
                 return -1;
-            if (-1==ioctl(f, DKIOCGETBLOCKSIZE, &bksize))
+            if (-1==ioctl(fh(), DKIOCGETBLOCKSIZE, &bksize))
                 return -1;
             return bkcount*bksize;
 #endif
 #ifdef BLKGETSIZE64
             uint64_t devsize;
-            if (-1==ioctl(f, BLKGETSIZE64, &devsize))
+            if (-1==ioctl(fh(), BLKGETSIZE64, &devsize))
                 return -1;
             return devsize;
 #endif
@@ -93,5 +113,89 @@ struct filehandle {
             // pipe or socket
             return -1;
         }
+    }
+
+    uint64_t seek(int64_t pos, int whence=SEEK_SET)
+    {
+        auto rc = ::lseek(fh(), pos, whence);
+        if (rc == -1)
+            throw std::system_error(errno, std::generic_category(), "lseek");
+        return rc;
+    }
+    uint64_t tell()
+    {
+        return seek(0, SEEK_CUR);
+    }
+    void trunc(uint64_t pos)
+    {
+        if (-1==::ftruncate(fh(), pos))
+            throw std::system_error(errno, std::generic_category(), "truncate");
+    }
+    template<typename RANGE>
+    auto write(const RANGE& r)
+    {
+        auto rc = ::write(fh(), &*r.begin(), r.size());
+        if (rc == -1)
+            throw std::system_error(errno, std::generic_category(), "write");
+        
+        return rc;
+    }
+    template<typename PTR>
+    auto write(PTR first, PTR last)
+    {
+        auto rc = ::write(fh(), first, std::distance(first, last)*sizeof(*first));
+        if (rc == -1)
+            throw std::system_error(errno, std::generic_category(), "write");
+        
+        return rc;
+    }
+    template<typename PTR>
+    auto write(PTR ptr, size_t count)
+    {
+        auto rc = ::write(fh(), ptr, count*sizeof(*ptr));
+        if (rc == -1)
+            throw std::system_error(errno, std::generic_category(), "write");
+        
+        return rc;
+    }
+    template<typename INT>
+    std::enable_if_t<std::is_scalar_v<INT>, std::vector<uint8_t>> read(INT count)
+    {
+        std::vector<uint8_t> data(count);
+        auto rc = ::read(fh(), &data[0], data.size());
+        if (rc == -1)
+            throw std::system_error(errno, std::generic_category(), "read");
+
+        data.resize(rc);
+
+        return data;
+    }
+    template<typename PTR>
+    size_t read(PTR first, PTR last)
+    {
+        auto rc = ::read(fh(), first, std::distance(first, last)*sizeof(*first));
+        if (rc == -1)
+            throw std::system_error(errno, std::generic_category(), "read");
+
+        return rc;
+    }
+    template<typename PTR>
+    size_t read(PTR ptr, size_t count)
+    {
+        auto rc = ::read(fh(), ptr, count*sizeof(*ptr));
+        if (rc == -1)
+            throw std::system_error(errno, std::generic_category(), "read");
+
+        return rc;
+    }
+
+    template<typename RANGE>
+    std::enable_if_t<!std::is_scalar_v<RANGE>, size_t> read(RANGE r)
+    {
+        auto rc = ::read(fh(), &*r.begin(), r.size() * sizeof(RANGE::value_type));
+        if (rc == -1)
+            throw std::system_error(errno, std::generic_category(), "read");
+
+        return rc;
     }
 };
