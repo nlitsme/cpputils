e552401c559d10a15f2cd922dbd2a6441f9dd682
willem <itsme@xs4all.nl>
2020-07-01 20:19:31 +0200

    crccalc: only keeping one instance of each calculator. separating out the add/calc functions

--- crccalc.h
+++ crccalc.h
@@ -35,41 +35,52 @@ public:
     {
         return table[(crc^b)&0xFF] ^ (crc>>8);
     }
-    INT calc(INT crc, const uint8_t *p, int size) const
+    INT add(INT crc, const uint8_t *p, int size) const
     {
-        constexpr INT mask = ((((INT)1<<(nbits-1))-1)<<1) | 1;
         while (size--)
             crc = add(crc, *p++);
-        return crc ^ mask;
+        return crc;
+    }
+
+    INT calc(const uint8_t *p, int size) const
+    {
+        constexpr INT mask = ((((INT)1<<(nbits-1))-1)<<1) | 1;
+        return add(mask, p, size) ^ mask;
     }
 };
 
-
-template<typename V>
-uint16_t crc16(uint16_t crc, const V& v)
-{
-    static CrcCalc<uint16_t, 0x8408, 16> CRC;
-    return CRC.calc(crc, &v[0], v.size());
-} 
 
 template<typename P>
 uint16_t crc16(uint16_t crc, P ptr, size_t size)
 {
     static CrcCalc<uint16_t, 0x8408, 16> CRC;
-    return CRC.calc(crc, ptr, size);
+    return CRC.add(crc, ptr, size);
+}
+template<typename P>
+uint16_t crc16(P ptr, size_t size)
+{
+    return crc16(~0, ptr, size) ^ (~0);
+}
+template<typename V>
+uint16_t crc16(const V& v)
+{
+    return crc16(&v[0], v.size());
 } 
 
-template<typename V>
-uint32_t crc32(uint32_t crc, const V& v)
-{
-    static CrcCalc<uint32_t, 0xEDB88320, 32> CRC;
-    return CRC.calc(crc, &v[0], v.size());
-} 
 
 template<typename P>
 uint32_t crc32(uint32_t crc, P ptr, size_t size)
 {
     static CrcCalc<uint32_t, 0xEDB88320, 32> CRC;
-    return CRC.calc(crc, ptr, size);
+    return CRC.add(crc, ptr, size);
+}
+template<typename P>
+uint32_t crc32(P ptr, size_t size)
+{
+    return crc32(~0, ptr, size) ^ (~0);
 } 
-
+template<typename V>
+uint32_t crc32(const V& v)
+{
+    return crc32(&v[0], v.size());
+} 
