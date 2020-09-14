be0396b486b61f9f3c8a6b6712606e8a3420d3ef
itsme <itsme@xs4all.nl>
2020-09-14 19:17:37 +0200

    added base32 support

---
+++ base32encoder.h
@@ -0,0 +1,150 @@
+#pragma once
+
+#include <tuple>
+#include <vector>
+#include <string>
+
+// encode a 5 byte chunk into 8 characters
+template<typename P, typename S>
+P base32_encode_chunk(P chunk, P last, S enc)
+{
+    static const char*b32ToChar= "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567=";
+
+    P p = chunk;
+    auto b = *p++;
+
+    int c[8] = { (b&0xF8)>>3, (b&0x07)<<2, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+
+    if (p < last) {
+        b = *p++;
+        c[1] |= (b&0xc0)>>6;
+        c[2] = (b&0x3e)>>1;
+        c[3] = (b&1)<<4;
+
+        if (p < last) {
+            b = *p++;
+            c[3] |= (b&0xf0)>>4;
+            c[4] = (b&0x0f)<<1;
+            if (p < last) {
+                b = *p++;
+                c[4] |= (b&0x80)>>7;
+                c[5] = (b&0x7c)>>2;
+                c[6] = (b&0x03)<<3;
+                if (p < last) {
+                    b = *p++;
+                    c[6] |= (b&0xe0)>>5;
+                    c[7] = (b&0x1f);
+                }
+            }
+        }
+    }
+
+    for (int i=0 ; i<8 ; i++)
+        *enc++= b32ToChar[c[i]];
+
+    return p;
+}
+template<typename P, typename S>
+std::tuple<P, S> base32_encode(P ifirst, P ilast, S ofirst, S olast)
+{
+    P p = ifirst;
+    S o = ofirst;
+    while (p < ilast && o+8 <= olast)
+    {
+        p = base32_encode_chunk(p, ilast, o);
+        o += 8;
+    }
+    return { p, o };
+}
+
+// -1 : invalid b32
+// -2 : b32 terminator
+// -3 : whitespace
+// >=0: valid b32
+static const int char2b32[]= {
+//  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-3,-3,-1,-3,-3,-1,-1, // 0
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // 1
+-3,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // 2
+-1,-1,26,27,28,29,30,31,-1,-1,-1,-1,-1,-2,-1,-1, // 3
+-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14, // 4
+15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1, // 5
+-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14, // 4
+15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1, // 5
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // 8
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // 9
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // a
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // b
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // c
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // d
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // e
+-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // f
+};
+template<typename S, typename P>
+std::tuple<S, P, bool> base32_decode(S ifirst, S ilast, P ofirst, P olast)
+{
+    S p = ifirst;
+    P o = ofirst;
+
+    uint8_t b = 0;
+    int i = 0;
+    while (p < ilast && o < olast)
+    {
+        auto c = *p++;
+        int cv = char2b32[(uint8_t)c];
+        if (cv==-2) {
+            // '=' is always a proper base32 ending
+            i = 0;
+            break;
+        }
+        else if (cv==-3) {
+            // skip whitespace
+            continue;
+        }
+        else if (cv==-1) {
+            // invalid character.
+            return { p, o, false };
+        }
+
+        // 00000111 11222223 33334444 45555566 66677777
+        switch(i)
+        {
+            case 0: b = cv<<3; i=1; break;
+            case 1: b |= cv>>2; *o++ = b; b = cv<<6; i=2; break;
+            case 2: b |= cv<<1; i=3; break;
+            case 3: b |= cv>>4; *o++ = b; b = cv<<4; i=4; break;
+            case 4: b |= cv>>1; *o++ = b; b = cv<<7; i=5; break;
+            case 5: b |= cv<<2; i=6; break;
+            case 6: b |= cv>>3; *o++ = b; b = cv<<5; i=7; break;
+            case 7: b |= cv; *o++ = b; i = 0; break;
+        }
+    }
+    // skip trailing '='
+    while (p < ilast && *p == '=')
+        p++;
+
+    return { p, o, i==0 };
+}
+
+template<typename S>
+std::vector<uint8_t> base32_decode(const S& txt)
+{
+    std::vector<uint8_t> data(((txt.size()+7)/8)*5);
+    auto [p, o, ok] = base32_decode(txt.begin(), txt.end(), data.begin(), data.end());
+    if (!ok)
+        throw std::runtime_error("base32_decode");
+    // todo: check if all data was decoded.
+    data.erase(o, data.end());
+    return data;
+}
+
+template<typename P>
+std::string base32_encode(const P& data)
+{
+    std::string txt(((data.size()+4)/5)*8, char(0));
+    auto [p, o] = base32_encode(data.begin(), data.end(), txt.begin(), txt.end());
+    // todo: check if all data was encoded.
+    txt.erase(o, txt.end());
+    return txt;
+}
+
---
+++ tests/test-base32.cpp
@@ -0,0 +1,91 @@
+#include "unittestframework.h"
+
+#include "base32encoder.h"
+#include "base32encoder.h"
+
+#include <array>
+
+TEST_CASE("base32") {
+    enum { E=1, D=2, FD=4 };
+    std::vector<std::tuple<std::vector<uint8_t>, int, std::string>> testcases = {
+        { { }, E|D, "" },
+        { { }, D,   "\n" },
+        { { }, D,   "\t" },
+        { { }, FD,   "." },
+        { { }, D,   "\t" },
+        { { }, D,   "=" },
+        { { }, D,   "==" },
+        { { }, D,   "===" },
+
+        // from rfc4648
+        { { 0x66                          }, E|D, "MY======" },
+        { { 0x66,0x6f                     }, E|D, "MZXQ====" },
+        { { 0x66,0x6f,0x6f                }, E|D, "MZXW6===" },
+        { { 0x66,0x6f,0x6f,0x62           }, E|D, "MZXW6YQ=" },
+        { { 0x66,0x6f,0x6f,0x62,0x61      }, E|D, "MZXW6YTB" },
+        { { 0x66,0x6f,0x6f,0x62,0x61,0x72 }, E|D, "MZXW6YTBOI======" },
+
+
+    };
+    SECTION("cases") {
+        for (auto& [data, flags, txt] : testcases) {
+            if (flags&E) {
+                auto e = base32_encode(data);
+                CHECK(e == txt);
+            }
+            if (flags&D) {
+                auto d = base32_decode(txt);
+                CHECK(d == data);
+            }
+            if (flags&FD) {
+                CHECK_THROWS(base32_decode(txt));
+            }
+        }
+    }
+    SECTION("types") {
+        std::array<uint8_t, 3> a = { 0, 1, 2 };
+        CHECK( base32_encode(a) == "AAAQE===" );
+
+        std::vector<uint8_t> v = { 0, 1, 2 };
+        CHECK( base32_encode(v) == "AAAQE===" );
+
+        std::basic_string<uint8_t> bs = { 0, 1, 2 };
+        CHECK( base32_encode(bs) == "AAAQE===" );
+        CHECK( base32_encode((std::basic_string_view<uint8_t>)bs ) == "AAAQE===" );
+
+        std::vector<int> iv = { 0, 1, 2 };
+        CHECK( base32_encode(iv) == "AAAQE===" );
+
+        std::string txt = "AAAQE===";
+        CHECK( base32_decode(txt) == std::vector<uint8_t>{0, 1, 2} );
+        CHECK( base32_decode((std::string_view)txt) == std::vector<uint8_t>{0, 1, 2} );
+
+        std::basic_string<int> itxt = { 'A', 'A', 'A', 'Q', 'E', '=', '=', '=' };
+
+        CHECK( base32_decode(itxt) == std::vector<uint8_t>{0, 1, 2} );
+        CHECK( base32_decode((std::basic_string_view<int>)itxt) == std::vector<uint8_t>{0, 1, 2} );
+
+        std::basic_string<uint8_t> btxt = { 'A', 'A', 'A', 'Q', 'E', '=', '=', '=' };
+        CHECK( base32_decode(btxt) == std::vector<uint8_t>{0, 1, 2} );
+        CHECK( base32_decode((std::basic_string_view<uint8_t>)btxt) == std::vector<uint8_t>{0, 1, 2} );
+    }
+    SECTION("invalid") {
+        std::string txt = "x";
+
+        for (int i = 0 ; i < 256 ; i++)
+        {
+            if (i == '\t' || i == '\n' || i == '\r' || i == 0xc || i == ' '
+                    || (i>='2' && i<='7')
+                    || (i>='a' && i<='z')
+                    || (i>='A' && i<='Z')
+                    || i=='=')
+                continue;
+            txt[0] = i;
+
+            INFO("checking char " << i);
+            CHECK_THROWS( base32_decode(txt) );
+        }
+    }
+}
+
+
