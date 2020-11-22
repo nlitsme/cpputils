4954d960b3ee43a8ccd7581f02bb22bcc99b3119
itsme <itsme@xs4all.nl>
2020-11-22 17:28:36 +0100

    alphabets for base32 and base64 coding

--- base64encoder.h
+++ base64encoder.h
@@ -3,14 +3,13 @@
 #include <tuple>
 #include <vector>
 #include <string>
+#include "b64-alphabet.h"
 
 // encode a 3 byte chunk into 4 characters
-template<typename P, typename S>
+template<typename P, typename S, typename ALPHABET=StandardBase64>
 P base64_encode_chunk(P chunk, P last, S enc)
 {
     // note: facebook, youtube use a modified version with  tr "+/"  "-_"
-    static const char*b642char= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-
     P p = chunk;
     auto b = *p++;
 
@@ -31,50 +30,27 @@ P base64_encode_chunk(P chunk, P last, S enc)
         }
     }
 
-    *enc++= b642char[c0];
-    *enc++= b642char[c1];
-    *enc++= b642char[c2];
-    *enc++= b642char[c3];
+    *enc++= ALPHABET::code2char(c0);
+    *enc++= ALPHABET::code2char(c1);
+    *enc++= ALPHABET::code2char(c2);
+    *enc++= ALPHABET::code2char(c3);
 
     return p;
 }
-template<typename P, typename S>
+template<typename P, typename S, typename ALPHABET=StandardBase64>
 std::tuple<P, S> base64_encode(P ifirst, P ilast, S ofirst, S olast)
 {
     P p = ifirst;
     S o = ofirst;
     while (p < ilast && o+4 <= olast)
     {
-        p = base64_encode_chunk(p, ilast, o);
+        p = base64_encode_chunk<P, S, ALPHABET>(p, ilast, o);
         o += 4;
     }
     return { p, o };
 }
 
-// -1 : invalid b64
-// -2 : b64 terminator
-// -3 : whitespace
-// >=0: valid b64
-static const int char2b64[]= {
-//  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f
--1,-1,-1,-1,-1,-1,-1,-1,-1,-3,-3,-1,-3,-3,-1,-1, // 0
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // 1
--3,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,62,-1,-1,-1,63, // 2
-52,53,54,55,56,57,58,59,60,61,-1,-1,-1,-2,-1,-1, // 3
--1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14, // 4
-15,16,17,18,19,20,21,22,23,24,25,-1,-1,-1,-1,-1, // 5
--1,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40, // 6
-41,42,43,44,45,46,47,48,49,50,51,-1,-1,-1,-1,-1, // 7
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // 8
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // 9
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // a
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // b
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // c
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // d
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // e
--1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1, // f
-};
-template<typename S, typename P>
+template<typename S, typename P, typename ALPHABET=StandardBase64>
 std::tuple<S, P, bool> base64_decode(S ifirst, S ilast, P ofirst, P olast)
 {
     S p = ifirst;
@@ -85,7 +61,7 @@ std::tuple<S, P, bool> base64_decode(S ifirst, S ilast, P ofirst, P olast)
     while (p < ilast && o < olast)
     {
         auto c = *p++;
-        int cv = char2b64[(uint8_t)c];
+        int cv = ALPHABET::char2code(c);
         if (cv==-2) {
             // '=' is always a proper base64 ending
             i = 0;
@@ -115,16 +91,22 @@ std::tuple<S, P, bool> base64_decode(S ifirst, S ilast, P ofirst, P olast)
     return { p, o, i==0 };
 }
 
-template<typename S>
-std::vector<uint8_t> base64_decode(const S& txt)
+template<typename P>
+std::vector<uint8_t> base64_decode(P first, P last)
 {
-    std::vector<uint8_t> data(((txt.size()+3)/4)*3);
-    auto [p, o, ok] = base64_decode(txt.begin(), txt.end(), data.begin(), data.end());
+    std::vector<uint8_t> data(((std::distance(first, last)+3)/4)*3);
+    auto [p, o, ok] = base64_decode(first, last, data.begin(), data.end());
     if (!ok)
         throw std::runtime_error("base64_decode");
     // todo: check if all data was decoded.
     data.erase(o, data.end());
     return data;
+}
+
+template<typename S>
+std::vector<uint8_t> base64_decode(const S& txt)
+{
+    return base64_decode(txt.begin(), txt.end());
 }
 
 template<typename P>
