ade19d34960290e18c1ec022784e3917788b237e
itsme <itsme@xs4all.nl>
2024-06-21 16:53:48 +0200

    removed asn1 parser

--- tests/unittests.cpp
+++ tests/unittests.cpp
@@ -3,7 +3,6 @@
 
 // including all here again, so we will catch linking errors.
 #include <cpputils/argparse.h>
-#include <cpputils/asn1parser.h>
 #include <cpputils/base64encoder.h>
 #include <cpputils/datapacking.h>
 #include <cpputils/fhandle.h>
